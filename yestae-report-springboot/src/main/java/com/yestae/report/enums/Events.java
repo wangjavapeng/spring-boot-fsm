e3191599f2c9e32348e53288b90610d0d27f8311
wangpeng <wangpeng@yestacom>
2019-03-27 15:41:26 +0800

    增加注释

--- yestae-report-springboot/src/main/java/com/yestae/report/enums/Events.java
+++ yestae-report-springboot/src/main/java/com/yestae/report/enums/Events.java
@@ -1,5 +1,11 @@
 package com.yestae.report.enums;
 
+/**
+ * 事件类型
+ * @author wangpeng
+ *
+ */
 public enum Events {
+//	投币	      开门
 	COIN, PUSH
 }
--- yestae-report-springboot/src/main/java/com/yestae/report/enums/States.java
+++ yestae-report-springboot/src/main/java/com/yestae/report/enums/States.java
@@ -1,5 +1,10 @@
 package com.yestae.report.enums;
 
+/**
+ * 
+ * 状态值
+ *
+ */
 public enum States {
 	LOCKED, UNLOCKED
 }
